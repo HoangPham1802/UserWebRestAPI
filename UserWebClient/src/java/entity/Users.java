41eaa567bc64df4ea0e7e7f3208b2068a94424d6
Violetevergarden <hoangpmd00265@fpt.edu.vn>
2018-08-22 23:41:42 +0700

    User Webservice Server/Client

---
+++ UserWebClient/src/java/entity/Users.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author hoang
+ */
+public class Users {
+    private Integer userid;
+    private String username;
+    private String password;
+    private String role;
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    
+    
+}
